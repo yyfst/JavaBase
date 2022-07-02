5d7db236aaed38a10b666b0f508cbf132f4da32c
yyfst <yyfjlyx@163.com>
2022-07-03 00:12:30 +0800

    java基础-饿汉模式

--- src/main/java/designpattern/singleton/HungerSingleton.java
+++ src/main/java/designpattern/singleton/HungerSingleton.java
@@ -2,8 +2,10 @@ package designpattern.singleton;
 
 /**
  * 饿汉模式，初始化的时候创建单例
+ * 使用类的时候才会初始化
  */
 public class HungerSingleton {
+    // 只会被实例化一次
     private static HungerSingleton instance = new HungerSingleton();
 
     private HungerSingleton() {
